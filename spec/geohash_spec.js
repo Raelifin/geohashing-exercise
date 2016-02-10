93c8bdbc9c6f03077ec25a398df9c7c9bcf693bc
Raelifin <raelifin@gmail.com>
2016-02-10 17:34:56 -0500

    buildGeoHash is now a pure function

--- spec/geohash_spec.js
+++ spec/geohash_spec.js
@@ -12,9 +12,11 @@ describe("Geohash", function() {
 			var sampleLat = '1.001';
 			var sampleLon = '2.002';
 			var result;
+			
 			//Act
 			runs(function() { subject(sampleLat, sampleLon, function(r) { result = r; }); });
 			waitsFor(function() { return result; }, "Result should exist", 1000);
+			
 			//Assert
 			runs(function() {
 				expect(result['lat']).toBeDefined();
@@ -28,11 +30,10 @@ describe("Geohash", function() {
 	describe('makeDateString', function() {
 		var subject = require('../lib/geohash').makeDateString;
 		it('returns a formatted date string', function() {
-			//Arrange
 			var date = new Date('December 17, 1995 03:24:00');
-			//Act
+			
 			var result = subject(date);
-			//Assert
+			
 			expect(result).toBe('1995-12-17');
 		});
 	});
