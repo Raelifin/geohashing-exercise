af6bda614ffe4cceccd8a9c8cc19405dc1ebb8ea
Raelifin <raelifin@gmail.com>
2016-02-12 16:14:40 -0500

    reportSiteContents is more strictly tested

--- spec/webcache_spec.js
+++ spec/webcache_spec.js
@@ -2,12 +2,12 @@ describe('Webcache', function() {
 	
 	describe('reportSiteContents', function() {
 		var subject = require('../lib/webcache').reportSiteContents;
-		it('returns a promise of a string', function(done) {
+		it('returns a string with text from example.org when asked for example.org', function(done) {
 			 var url = 'http://example.org';
 			 
 			 subject(url).then(function(result) {
 				 
-				 expect(typeof result).toBe('string');
+				 expect(result).toMatch(/<p>This domain is established to be used for illustrative examples in documents/);
 				 
 				 done();
 			 });
