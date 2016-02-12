305a3b77063d91fb8b1730a4045f5951fee136a2
Raelifin <raelifin@gmail.com>
2016-02-12 17:35:59 -0500

    cacheable no longer buggy and cacheableWithExpiration now exists

--- spec/webcache_spec.js
+++ spec/webcache_spec.js
@@ -24,15 +24,57 @@ describe('Webcache', function() {
 		});
 	});
 	
+	/**
+	 * Max's learning note:
+	 * In the Arrange->Act->Assert paradigm, does a test of a higher-order function treat
+	 * the first-order function as the result, or does it test the data it feeds the
+	 * first-order function as the result? I'm a bit confused here.
+	 **/
+	
 	describe('cacheable', function() {
 		var subject = require('../lib/webcache').cacheable;
+		it ('creates can create different cacheable functions on different keys', function() {
+			var key1 = 42;
+			var key2 = 1337;
+			
+			var result1 = subject(x => Math.random()*x, key1);
+			var result2 = subject(x => Math.random()*Math.random(), key2);
+			
+			expect(result1(key1)).toBe(result1(key1));
+			expect(result1(-key1)).not.toBe(result1(-key1));
+			expect(result2(key2)).toBe(result2(key2));
+			expect(result2(-key2)).not.toBe(result2(-key2));
+			expect(result1(key1)).not.toBe(result2(key2));
+		});
+	});
+	
+	describe('cacheableWithExpiration', function() {
+		var subject = require('../lib/webcache').cacheableWithExpiration;
 		it ('creates a cacheable function on a specific key', function() {
-			var key = 37;
+			var key = 42;
 			
 			var result = subject(x => Math.random()*x, key);
 			
 			expect(result(key)).toBe(result(key));
 			expect(result(-key)).not.toBe(result(-key));
+		});
+		/**
+		 * Max's learning note:
+		 * I'm really curious how to best handle the Arrange->Act->Assert paradigm in weird tests like this one.
+		 * Is this "right"?
+		 **/
+		it ('expires in the appropriate amount of time', function(done) {
+			var key = 42;
+			
+			var result = subject(x => Math.random()*x, key, 0.5);
+			var output1 = result(key);
+			setTimeout(function() {
+				var output2 = result(key);
+				
+				expect(output1).not.toBe(output2);
+				
+				done();
+			}, 1000);
 		});
 	});
 });
